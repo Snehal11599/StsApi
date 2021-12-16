aefa87c92871bcc24a7e06b781f4e714809ff26c
Snehal11599 <snehalc11599@gmail.com>
2021-12-16 00:34:00 -0800

    initil commit

---
+++ src/main/java/com/test/testApplication/dto/StudentDto.java
@@ -0,0 +1,20 @@
+package com.test.testApplication.dto;
+
+import lombok.Data;
+
+@Data
+public class StudentDto {
+	
+	private long id;
+	
+	private String Firstname;
+	
+	private String Lastname;
+	
+	private String email;
+	
+	private String password;
+	
+	
+
+}
