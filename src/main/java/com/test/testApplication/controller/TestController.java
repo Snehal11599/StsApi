32549645c678bcfa8b998a2f84898ec9cbfa888b
Snehal11599 <snehalc11599@gmail.com>
2021-12-20 23:37:27 -0800

    api

--- src/main/java/com/test/testApplication/controller/TestController.java
+++ src/main/java/com/test/testApplication/controller/TestController.java
@@ -1,7 +1,6 @@
-package com.test.testApplication.controller;
+package com.test.testApplication.Controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,38 +10,47 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.test.testApplication.controller.entity.Student;
 import com.test.testApplication.dto.StudentDto;
 import com.test.testApplication.dto.TestResponse;
+import com.test.testApplication.entity.Student;
 import com.test.testApplication.service.TestService;
-
-
 
 @RestController
 @RequestMapping("/hello")
-public class TestController {
-	
-	@Autowired
-	TestService testService;
-	
-	@PostMapping(value = "/save")
-	public ResponseEntity<TestResponse<?>>saveUsers(@RequestBody StudentDto studentDto){
-		TestResponse<StudentDto> testResponse = new TestResponse<>();
-		Student savedUserResponse = testService.save(studentDto);
-	
-		testResponse.setHttpsStatus (HttpStatus.OK);
-		testResponse.setMessage ("saves successfully");
-		testResponse.setData(savedUserResponse);
-        return new ResponseEntity<>( testResponse  , HttpStatus.OK);
-		
-	}
-	 @GetMapping("/getusers")
-	 
-	 public List <Student> getStudent() {
-		
-		 return this.testService.getStudent();
-	 }
-}
-	 
 
+
+public class TestController {
+
+@Autowired
+
+ private TestService testService;
+
+
+@PostMapping(value = "/save")
+public ResponseEntity<TestResponse<?>>saveUsers(@RequestBody StudentDto studentDto){
+TestResponse<StudentDto> testResponse = new TestResponse<>();
+StudentDto savedUserResponse = testService.save(studentDto);
+testResponse.setHttpsStatus (HttpStatus.OK);
+testResponse.setMessage ("saves successfully");
+testResponse.setData(savedUserResponse);
+        return new ResponseEntity<>( testResponse  , HttpStatus.OK);
+}
+
+
+@GetMapping ("/getUsers")
+
+public List<StudentDto> getAllList() {
+	return this.testService.getAllList();
+}
+	
+	
+}
+
+
+
+        
+       
+
+
+	
 
